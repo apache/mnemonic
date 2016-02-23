22288f8f51aee54aa9ad23f824ff1a25731b8c66
Wang, Gang(Gary) <gang1.wang@intel.com>
2016-02-23 13:20:20 -0800

    added comments to Utils and re-formatted annotation classes

--- core/src/main/java/com/intel/bigdatamem/AnnotationProcessingException.java
+++ core/src/main/java/com/intel/bigdatamem/AnnotationProcessingException.java
@@ -1,25 +1,23 @@
 package com.intel.bigdatamem;
 
-/**
- *
- *
- */
-
-
 import javax.lang.model.element.Element;
 
+/**
+ * this exception is used for non-volatile annotation processing 
+ *
+ */
 public class AnnotationProcessingException extends Exception {
 
-	private static final long serialVersionUID = 6911141027622831646L;
+    private static final long serialVersionUID = 6911141027622831646L;
 
-	private Element element;
+    private Element element;
 
-	public AnnotationProcessingException(Element element, String msg, Object... args) {
-		super(String.format(msg, args));
-		this.element = element;
-	}
+    public AnnotationProcessingException(Element element, String msg, Object... args) {
+	super(String.format(msg, args));
+	this.element = element;
+    }
 
-	public Element getElement() {
-		return element;
-	}
+    public Element getElement() {
+	return element;
+    }
 }
