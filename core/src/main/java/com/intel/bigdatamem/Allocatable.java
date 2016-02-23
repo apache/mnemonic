87c359818add20ccf01dc780ec2dde2a7ac1a121
Wang, Gang(Gary) <gang1.wang@intel.com>
2016-02-23 12:11:32 -0800

    added comments for Allocatable and re-format allocators

--- core/src/main/java/com/intel/bigdatamem/Allocatable.java
+++ core/src/main/java/com/intel/bigdatamem/Allocatable.java
@@ -1,75 +1,118 @@
-
 package com.intel.bigdatamem;
 
 /**
- * a interface to allocate big memory resource from any underlying memory kind
+ * an interface to allocate memory resources from any underlying memory kind
  * of storage.
  * 
- *
  */
 public interface Allocatable<A extends CommonAllocator<A>> {
 	
-	/**
-	 * create a MemChunkHolder object along with a memory chunk that is
-	 * allocated from backed native memory pool.
-	 * 
-	 * @param <A> the type of bound allocator 
-	 * 
-	 * @param size
-	 *            specify the size of memory chunk
-	 * 
-	 * @param autoreclaim
-	 * 	          specify whether to reclaim its managed
-	 *            chunk automatically
-	 *
-	 * @return a created MemChunkHolder object
-	 * @see BigDataMemAllocator#createChunk(long)
-	 */
-	public MemChunkHolder<A> createChunk(long size, boolean autoreclaim);
+    /**
+     * create a memory chunk that is managed by its holder.
+     * 
+     * @param <A> the type of bound allocator 
+     * 
+     * @param size
+     *            specify the size of memory chunk
+     * 
+     * @param autoreclaim
+     * 	          specify whether or not to reclaim this
+     *            chunk automatically
+     *
+     * @return a holder contains a memory chunk
+     */
+    public MemChunkHolder<A> createChunk(long size, boolean autoreclaim);
 
-	public MemChunkHolder<A> createChunk(long size);
+    /**
+     * create a memory chunk that is managed by its holder.
+     * 
+     * @param <A> the type of bound allocator 
+     * 
+     * @param size
+     *            specify the size of memory chunk
+     * 
+     * @return a holder contains a memory chunk
+     */
+    public MemChunkHolder<A> createChunk(long size);
 
-	/**
-	 * create a MemBufferHolder object along with a ByteBuffer which is backed
-	 * with a buffer allocated from backed native memory pool.
-	 * 
-	 * @param <A> the type of bound allocator 
-	 * 
-	 * @param size
-	 *            specify the size of backed memory buffer
-	 * 
-	 * @param autoreclaim
-	 * 	          specify whether to reclaim its managed
-	 *            buffer automatically
-	 * 
-	 * @return a created MemBufferHolder object
-	 *
-	 * @see BigDataMemAllocator#createBuffer(long)
-	 */
-	public  MemBufferHolder<A> createBuffer(long size, boolean autoreclaim);
+    /**
+     * create a memory buffer that is managed by its holder.
+     * 
+     * @param <A> the type of bound allocator 
+     * 
+     * @param size
+     *            specify the size of memory buffer
+     * 
+     * @param autoreclaim
+     * 	          specify whether or not to reclaim this
+     *            buffer automatically
+     *
+     * @return a holder contains a memory buffer
+     */
+    public  MemBufferHolder<A> createBuffer(long size, boolean autoreclaim);
 	
-	public  MemBufferHolder<A> createBuffer(long size);
+    /**
+     * create a memory buffer that is managed by its holder.
+     * 
+     * @param <A> the type of bound allocator 
+     * 
+     * @param size
+     *            specify the size of memory buffer
+     * 
+     * @return a holder contains a memory buffer
+     */
+    public  MemBufferHolder<A> createBuffer(long size);
 
-	public void registerChunkAutoReclaim(MemChunkHolder<A> mholder);
+    /**
+     * register a memory chunk for auto-reclaim
+     *
+     * @param <A> the type of bound allocator 
+     * 
+     * @param mholder
+     *           specify a chunk holder to register
+     */
+    public void registerChunkAutoReclaim(MemChunkHolder<A> mholder);
 
-	public void registerBufferAutoReclaim(MemBufferHolder<A> mholder);
+    /**
+     * register a memory buffer for auto-reclaim
+     *
+     * @param <A> the type of bound allocator 
+     * 
+     * @param mholder
+     *           specify a buffer holder to register
+     */
+    public void registerBufferAutoReclaim(MemBufferHolder<A> mholder);
 
-	/**
-	 * reallocate a specified size of memory block from backed memory pool.
-	 * 
-	 * @param address
-	 *            the address of previous allocated memory block. it can be
-	 *            null.
-	 * 
-	 * @param size
-	 *            specify new size of memory block to be reallocated
-	 * 
-	 * @return    the address of reallocated memory block
-	 * 
-	* @see BigDataMemAllocator#reallocate(long, long)
-	*/
-	public MemChunkHolder<A> resizeChunk(MemChunkHolder<A> mholder, long size);
+    /**
+     * resize a memory chunk.
+     * 
+     * @param <A> the type of bound allocator 
+     * 
+     * @param mholder
+     *           specify a chunk holder for resizing
+     * 
+     * @param size
+     *            specify a new size of this memory chunk
+     * 
+     * @return the resized memory chunk holder
+     * 
+     */
+    public MemChunkHolder<A> resizeChunk(MemChunkHolder<A> mholder, long size);
 	
-	public MemBufferHolder<A> resizeBuffer(MemBufferHolder<A> mholder, long size);
+    /**
+     * resize a memory buffer.
+     * 
+     * @param <A> the type of bound allocator 
+     * 
+     * @param mholder
+     *           specify a buffer holder for resizing
+     * 
+     * @param size
+     *            specify a new size of this memory buffer
+     * 
+     * @return the resized memory buffer holder
+     * 
+     */
+    public MemBufferHolder<A> resizeBuffer(MemBufferHolder<A> mholder, long size);
 
 }
--- core/src/main/java/com/intel/bigdatamem/BigDataMemAllocator.java
+++ core/src/main/java/com/intel/bigdatamem/BigDataMemAllocator.java
@@ -13,252 +13,252 @@ import com.intel.mnemonic.service.allocatorservice.VolatileMemoryAllocatorServic
  */
 public class BigDataMemAllocator extends CommonAllocator<BigDataMemAllocator> {
 
-	private boolean m_activegc = true;
-	private long m_gctimeout = 100;
-	private long m_nid = -1;
-        private VolatileMemoryAllocatorService m_vmasvc = null;
+    private boolean m_activegc = true;
+    private long m_gctimeout = 100;
+    private long m_nid = -1;
+    private VolatileMemoryAllocatorService m_vmasvc = null;
 
-	/**
-	 * Constructor, it initializes and allocate a memory pool from specified uri
-	 * location with specified capacity and an allocator service instance. 
-	 * usually, the uri points to a mounted
-	 * memory device or a location of file system.
-	 * 
-	 * @param vmasvc
-	 *            the volatile memory allocation service instance
-	 *
-	 * @param capacity
-	 *            the capacity of memory pool
-	 * 
-	 * @param uri
-	 *            the location of memory pool will be created
-	 * 
-	 * @param isnew
-	 *            a place holder, always specify it as true
-	 */
-         public BigDataMemAllocator(VolatileMemoryAllocatorService vmasvc, long capacity, String uri, boolean isnew) {
-                assert null != vmasvc : "VolatileMemoryAllocatorService object is null";
+    /**
+     * Constructor, it initializes and allocate a memory pool from specified uri
+     * location with specified capacity and an allocator service instance. 
+     * usually, the uri points to a mounted
+     * memory device or a location of file system.
+     * 
+     * @param vmasvc
+     *            the volatile memory allocation service instance
+     *
+     * @param capacity
+     *            the capacity of memory pool
+     * 
+     * @param uri
+     *            the location of memory pool will be created
+     * 
+     * @param isnew
+     *            a place holder, always specify it as true
+     */
+    public BigDataMemAllocator(VolatileMemoryAllocatorService vmasvc, long capacity, String uri, boolean isnew) {
+	assert null != vmasvc : "VolatileMemoryAllocatorService object is null";
                 
-                m_vmasvc = vmasvc;
-		m_nid = m_vmasvc.init(capacity, uri, isnew);
+	m_vmasvc = vmasvc;
+	m_nid = m_vmasvc.init(capacity, uri, isnew);
 		
-		/**
-		 * create a resource collector to release specified bytebuffer that backed
-		 * by underlying big memory pool.
-		 */
-		m_bufcollector = new ResCollector<MemBufferHolder<BigDataMemAllocator>, ByteBuffer>(
-				new ResReclaim<ByteBuffer>() {
-					@Override
-					public void reclaim(ByteBuffer mres) {
-						boolean cb_reclaimed = false;
-						if (null != m_bufferreclaimer) {
-							cb_reclaimed = m_bufferreclaimer.reclaim(mres, Long.valueOf(mres.capacity()));
-						}
-						if (!cb_reclaimed) {
-							m_vmasvc.destroyByteBuffer(m_nid, mres);
-							mres = null;
-						}
-					}
-				});
-
-		/**
-		 * create a resource collector to release specified chunk that backed by
-		 * underlying big memory pool.
-		 */
-		m_chunkcollector = new ResCollector<MemChunkHolder<BigDataMemAllocator>, Long>(
-				new ResReclaim<Long>() {
-					@Override
-					public void reclaim(Long mres) {
-						// System.out.println(String.format("Reclaim: %X", mres));
-						boolean cb_reclaimed = false;
-						if (null != m_chunkreclaimer) {
-							cb_reclaimed = m_chunkreclaimer.reclaim(mres, null);
-						}
-						if (!cb_reclaimed) {
-							m_vmasvc.free(m_nid, mres);
-							mres = null;
-						}
-					}
-				});
-	}
-
-        /**
-         * enable active garbage collection. the GC will be forced to collect garbages when
-         * there is no more space for current allocation request.
-         *
-         * @param timeout
-         *            the timeout is used to yield for GC performing
-         */
-        @Override
-	public BigDataMemAllocator enableActiveGC(long timeout) {
-		m_activegc = true;
-		m_gctimeout = timeout;
-		return this;
-	}
+	/**
+	 * create a resource collector to release specified bytebuffer that backed
+	 * by underlying big memory pool.
+	 */
+	m_bufcollector = new ResCollector<MemBufferHolder<BigDataMemAllocator>, ByteBuffer>(
+											    new ResReclaim<ByteBuffer>() {
+												@Override
+												public void reclaim(ByteBuffer mres) {
+												    boolean cb_reclaimed = false;
+												    if (null != m_bufferreclaimer) {
+													cb_reclaimed = m_bufferreclaimer.reclaim(mres, Long.valueOf(mres.capacity()));
+												    }
+												    if (!cb_reclaimed) {
+													m_vmasvc.destroyByteBuffer(m_nid, mres);
+													mres = null;
+												    }
+												}
+											    });
 
 	/**
-	 * disable active garbage collection.
-	 * 
+	 * create a resource collector to release specified chunk that backed by
+	 * underlying big memory pool.
 	 */
-	@Override
-	public BigDataMemAllocator disableActiveGC() {
-		m_activegc = false;
-		return this;
-	}
+	m_chunkcollector = new ResCollector<MemChunkHolder<BigDataMemAllocator>, Long>(
+										       new ResReclaim<Long>() {
+											   @Override
+											   public void reclaim(Long mres) {
+											       // System.out.println(String.format("Reclaim: %X", mres));
+											       boolean cb_reclaimed = false;
+											       if (null != m_chunkreclaimer) {
+												   cb_reclaimed = m_chunkreclaimer.reclaim(mres, null);
+											       }
+											       if (!cb_reclaimed) {
+												   m_vmasvc.free(m_nid, mres);
+												   mres = null;
+											       }
+											   }
+										       });
+    }
 
-	/**
-	 * Release the memory pool and close it.
-	 */
-	@Override
-	public void close() {
-		super.close();
-	}
+    /**
+     * enable active garbage collection. the GC will be forced to collect garbages when
+     * there is no more space for current allocation request.
+     *
+     * @param timeout
+     *            the timeout is used to yield for GC performing
+     */
+    @Override
+    public BigDataMemAllocator enableActiveGC(long timeout) {
+	m_activegc = true;
+	m_gctimeout = timeout;
+	return this;
+    }
 
-	/**
-	 * force to synchronize uncommitted data to backed memory pool
-	 * (this is placeholder).
-	 *
-	 */
-	@Override
-	public void sync() {
-	}
+    /**
+     * disable active garbage collection.
+     * 
+     */
+    @Override
+    public BigDataMemAllocator disableActiveGC() {
+	m_activegc = false;
+	return this;
+    }
 
-	/**
-	 * reallocate a specified size of memory block from backed memory pool.
-	 * 
-	 * @param address
-	 *            the address of previous allocated memory block. it can be
-	 *            null.
-	 * 
-	 * @param size
-	 *            specify new size of memory block to be reallocated
-	 * 
-	 * @return the address of reallocated memory block from native memory pool
-	 */
-	@Override
-	public MemChunkHolder<BigDataMemAllocator> resizeChunk(MemChunkHolder<BigDataMemAllocator> mholder, long size){
-		MemChunkHolder<BigDataMemAllocator> ret = null;
-		boolean ac = null != mholder.getRefId();
-		if (size > 0) {
-			Long addr = m_vmasvc.reallocate(m_nid, mholder.get(), size, true);
-			if (0 == addr && m_activegc) {
-				forceGC();
-				addr = m_vmasvc.reallocate(m_nid, mholder.get(), size, true);
-			}
-			if (0 != addr) {
-				mholder.clear();
-				mholder.destroy();
-				ret = new MemChunkHolder<BigDataMemAllocator>(this, addr, size);
-				if (ac) {
-					m_chunkcollector.register(ret);
-				}
-			}
+    /**
+     * Release the memory pool and close it.
+     */
+    @Override
+    public void close() {
+	super.close();
+    }
+
+    /**
+     * force to synchronize uncommitted data to backed memory pool
+     * (this is placeholder).
+     *
+     */
+    @Override
+    public void sync() {
+    }
+
+    /**
+     * reallocate a specified size of memory block from backed memory pool.
+     * 
+     * @param address
+     *            the address of previous allocated memory block. it can be
+     *            null.
+     * 
+     * @param size
+     *            specify new size of memory block to be reallocated
+     * 
+     * @return the address of reallocated memory block from native memory pool
+     */
+    @Override
+    public MemChunkHolder<BigDataMemAllocator> resizeChunk(MemChunkHolder<BigDataMemAllocator> mholder, long size){
+	MemChunkHolder<BigDataMemAllocator> ret = null;
+	boolean ac = null != mholder.getRefId();
+	if (size > 0) {
+	    Long addr = m_vmasvc.reallocate(m_nid, mholder.get(), size, true);
+	    if (0 == addr && m_activegc) {
+		forceGC();
+		addr = m_vmasvc.reallocate(m_nid, mholder.get(), size, true);
+	    }
+	    if (0 != addr) {
+		mholder.clear();
+		mholder.destroy();
+		ret = new MemChunkHolder<BigDataMemAllocator>(this, addr, size);
+		if (ac) {
+		    m_chunkcollector.register(ret);
 		}
-		return ret;
+	    }
 	}
+	return ret;
+    }
 	
-        /**
-	 * resize a specified buffer on its backed memory pool.
-	 *
-	 * @param holder
-	 *            the holder of memory buffer. it can be
-	 *            null.
-	 * 
-	 * @param size
-	 *            specify a new size of memory chunk
-	 * 
-	 * @return the resized memory buffer handler
-	 *
-	 */
-	@Override
-	public MemBufferHolder<BigDataMemAllocator> resizeBuffer(MemBufferHolder<BigDataMemAllocator> mholder, long size) {
-		MemBufferHolder<BigDataMemAllocator> ret = null;
-		boolean ac = null != mholder.getRefId();
-		if (size > 0) {
-			int bufpos = mholder.get().position();
-			int buflimit = mholder.get().limit();
-			ByteBuffer buf = m_vmasvc.resizeByteBuffer(m_nid, mholder.get(), size);
-			if (null == buf && m_activegc) {
-				forceGC();
-				buf = m_vmasvc.resizeByteBuffer(m_nid, mholder.get(), size);
-			}
-			if (null != buf) {
-				mholder.clear();
-				mholder.destroy();
-				buf.position(bufpos <= size ? bufpos : 0);
-				buf.limit(buflimit <= size ? buflimit : (int)size);
-				ret = new MemBufferHolder<BigDataMemAllocator>(this, buf);
-				if (ac) {
-					m_bufcollector.register(ret);
-				}
-			}
+    /**
+     * resize a specified buffer on its backed memory pool.
+     *
+     * @param holder
+     *            the holder of memory buffer. it can be
+     *            null.
+     * 
+     * @param size
+     *            specify a new size of memory chunk
+     * 
+     * @return the resized memory buffer handler
+     *
+     */
+    @Override
+    public MemBufferHolder<BigDataMemAllocator> resizeBuffer(MemBufferHolder<BigDataMemAllocator> mholder, long size) {
+	MemBufferHolder<BigDataMemAllocator> ret = null;
+	boolean ac = null != mholder.getRefId();
+	if (size > 0) {
+	    int bufpos = mholder.get().position();
+	    int buflimit = mholder.get().limit();
+	    ByteBuffer buf = m_vmasvc.resizeByteBuffer(m_nid, mholder.get(), size);
+	    if (null == buf && m_activegc) {
+		forceGC();
+		buf = m_vmasvc.resizeByteBuffer(m_nid, mholder.get(), size);
+	    }
+	    if (null != buf) {
+		mholder.clear();
+		mholder.destroy();
+		buf.position(bufpos <= size ? bufpos : 0);
+		buf.limit(buflimit <= size ? buflimit : (int)size);
+		ret = new MemBufferHolder<BigDataMemAllocator>(this, buf);
+		if (ac) {
+		    m_bufcollector.register(ret);
 		}
-		return ret;
+	    }
 	}
+	return ret;
+    }
 
-	/**
-	 * create a MemChunkHolder object along with a memory chunk that is
-	 * allocated from backed native memory pool.
-	 * 
-	 * @param size
-	 *            specify the size of memory chunk
-	 * 
-	 * @return a created MemChunkHolder object
-	 */
-	@Override
-	public MemChunkHolder<BigDataMemAllocator> createChunk(long size, boolean autoreclaim) {
-		MemChunkHolder<BigDataMemAllocator> ret = null;
-		Long addr = m_vmasvc.allocate(m_nid, size, true);
-		if (0 == addr && m_activegc) {
-			forceGC();
-			addr = m_vmasvc.allocate(m_nid, size, true);
-		}
-		if (0 != addr) {
-			ret = new MemChunkHolder<BigDataMemAllocator>(this, addr, size);
-			ret.setCollector(m_chunkcollector);
-			if (autoreclaim) {
-				m_chunkcollector.register(ret);
-			}
-		}
-		return ret;
+    /**
+     * create a MemChunkHolder object along with a memory chunk that is
+     * allocated from backed native memory pool.
+     * 
+     * @param size
+     *            specify the size of memory chunk
+     * 
+     * @return a created MemChunkHolder object
+     */
+    @Override
+    public MemChunkHolder<BigDataMemAllocator> createChunk(long size, boolean autoreclaim) {
+	MemChunkHolder<BigDataMemAllocator> ret = null;
+	Long addr = m_vmasvc.allocate(m_nid, size, true);
+	if (0 == addr && m_activegc) {
+	    forceGC();
+	    addr = m_vmasvc.allocate(m_nid, size, true);
 	}
+	if (0 != addr) {
+	    ret = new MemChunkHolder<BigDataMemAllocator>(this, addr, size);
+	    ret.setCollector(m_chunkcollector);
+	    if (autoreclaim) {
+		m_chunkcollector.register(ret);
+	    }
+	}
+	return ret;
+    }
 
-	/**
-	 * create a memory buffer that is managed by its holder.
-	 * 
-	 * @param size
-	 *            specify the size of memory buffer
-	 * 
-	 * @return a holder contains a memory buffer
-	 */
-	@Override
-	public MemBufferHolder<BigDataMemAllocator> createBuffer(long size, boolean autoreclaim) {
-		MemBufferHolder<BigDataMemAllocator> ret = null;
-		ByteBuffer bb = m_vmasvc.createByteBuffer(m_nid, size);
-		if (null == bb && m_activegc) {
-			forceGC();
-			bb = m_vmasvc.createByteBuffer(m_nid, size);
-		}
-		if (null != bb) {
-			ret = new MemBufferHolder<BigDataMemAllocator>(this, bb);
-			ret.setCollector(m_bufcollector);
-			if (autoreclaim) {
-				m_bufcollector.register(ret);
-			}
-		}
-		return ret;
+    /**
+     * create a memory buffer that is managed by its holder.
+     * 
+     * @param size
+     *            specify the size of memory buffer
+     * 
+     * @return a holder contains a memory buffer
+     */
+    @Override
+    public MemBufferHolder<BigDataMemAllocator> createBuffer(long size, boolean autoreclaim) {
+	MemBufferHolder<BigDataMemAllocator> ret = null;
+	ByteBuffer bb = m_vmasvc.createByteBuffer(m_nid, size);
+	if (null == bb && m_activegc) {
+	    forceGC();
+	    bb = m_vmasvc.createByteBuffer(m_nid, size);
 	}
+	if (null != bb) {
+	    ret = new MemBufferHolder<BigDataMemAllocator>(this, bb);
+	    ret.setCollector(m_bufcollector);
+	    if (autoreclaim) {
+		m_bufcollector.register(ret);
+	    }
+	}
+	return ret;
+    }
 	
-	/**
-	 * force to perform GC that is used to release unused backed memory
-	 * resources.
-	 */
-	private void forceGC() {
-		System.gc();
-		try {
-			Thread.sleep(m_gctimeout);
-		} catch (Exception ex) {
-		}
+    /**
+     * force to perform GC that is used to release unused backed memory
+     * resources.
+     */
+    private void forceGC() {
+	System.gc();
+	try {
+	    Thread.sleep(m_gctimeout);
+	} catch (Exception ex) {
 	}
+    }
 
 }
