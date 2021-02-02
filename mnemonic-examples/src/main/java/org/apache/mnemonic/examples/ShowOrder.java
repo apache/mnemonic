fbb7db75c66ea6163d7049bfc4da6e20a23f9e89
Xiaojin Jiao <xj.jiao@gmail.com>
2021-02-01 21:32:12 -0800

    MNEMONIC-599: Replace the Utils.getVolatileMemoryAllocatorService call from Example
    Signed-off-by: Xiaojin Jiao <xj.jiao@gmail.com>

--- mnemonic-examples/src/main/java/org/apache/mnemonic/examples/ShowOrder.java
+++ mnemonic-examples/src/main/java/org/apache/mnemonic/examples/ShowOrder.java
@@ -21,14 +21,14 @@ import org.apache.mnemonic.DurableType;
 import org.apache.mnemonic.EntityFactoryProxy;
 import org.apache.mnemonic.EntityFactoryProxyHelper;
 import org.apache.mnemonic.NonVolatileMemAllocator;
-import org.apache.mnemonic.Utils;
+import org.apache.mnemonic.service.memory.internal.PMemServiceImpl;
 
 public class ShowOrder {
 
   public static void main(String[] argv) throws Exception {
 
     /* create a non-volatile memory pool from one of memory services */
-    NonVolatileMemAllocator act = new NonVolatileMemAllocator(Utils.getNonVolatileMemoryAllocatorService("pmalloc"),
+    NonVolatileMemAllocator act = new NonVolatileMemAllocator(new PMemServiceImpl(),
         1024L * 1024 * 1024, "./example_order.dat", false);
 
     DurableType listgftypes[] = {DurableType.DURABLE};
