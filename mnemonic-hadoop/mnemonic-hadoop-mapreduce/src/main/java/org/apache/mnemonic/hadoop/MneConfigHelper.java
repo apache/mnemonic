b49bc3ecceaf2d78d2753c32ddc024c5e4cbdb2e
Yanhui Zhao <yzhao@apache.org>
2021-08-12 22:47:05 -0700

    MNEMONIC-672: Update Hadoop integration version to latest stable version of 3.3.1

--- mnemonic-hadoop/mnemonic-hadoop-mapreduce/src/main/java/org/apache/mnemonic/hadoop/MneConfigHelper.java
+++ mnemonic-hadoop/mnemonic-hadoop-mapreduce/src/main/java/org/apache/mnemonic/hadoop/MneConfigHelper.java
@@ -21,7 +21,7 @@ package org.apache.mnemonic.hadoop;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.mnemonic.ConfigurationException;
 import org.apache.mnemonic.DurableType;
