05991c79da09fdcd073bec281f9cbdce608fd40c
Zhen Li <rhythmwind@gmail.com>
2021-10-09 22:55:19 -0700

    MNEMONIC-691: Fix javadoc error exposed by JDK15
    Signed-off-by: Zhen Li <rhythmwind@gmail.com>

--- mnemonic-core/src/main/java/org/apache/mnemonic/EvictFilter.java
+++ mnemonic-core/src/main/java/org/apache/mnemonic/EvictFilter.java
@@ -19,22 +19,22 @@ package org.apache.mnemonic;
 
 /**
  * A listener callback to validate its accepted key-value pair for evacuation.
- * 
+ *
  */
 public interface EvictFilter<KeyT, ValueT> {
   /**
    * A call-back validator when an entry will be evicted.
-   * 
+   *
    * @param pool
    *          the pool which an entry has been evicted from
-   * 
+   *
    * @param k
    *          the entry's key
-   * 
+   *
    * @param v
    *          the entry's value
-   * 
-   * @return <tt>true</tt> if the entry is allowed to be dropped from its cache
+   *
+   * @return <code>true</code> if the entry is allowed to be dropped from its cache
    *         pool.
    */
   boolean validate(CachePool<KeyT, ValueT> pool, KeyT k, ValueT v);
