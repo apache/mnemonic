d21701f7eadc073d843aed741c65701fa66ad375
Katarina <katarinaking361@gmail.com>
2023-09-03 04:39:30 +0000

    Mnemonic-795: Change String into Entry

--- mnemonic-core/src/main/java/org/apache/mnemonic/AnnotatedDurableEntityClass.java
+++ mnemonic-core/src/main/java/org/apache/mnemonic/AnnotatedDurableEntityClass.java
@@ -807,8 +807,8 @@ public class AnnotatedDurableEntityClass {
         switch (name) {
           case "cancelAutoReclaim":
             code.addStatement("$1N.cancelAutoReclaim()", holdername);
-            for (String fname : m_dynfieldsinfo.keySet()) {
-              dynfieldinfo = m_dynfieldsinfo.get(fname);
+            for (Map.Entry<String, FieldInfo> entry : m_dynfieldsinfo.entrySet()) {
+              dynfieldinfo = entry.getValue();
               if (!isUnboxPrimitive(dynfieldinfo.type)) {
                 code.beginControlFlow("if (null != $1N)", dynfieldinfo.name);
                 code.addStatement("$1N.cancelAutoReclaim()", dynfieldinfo.name);
