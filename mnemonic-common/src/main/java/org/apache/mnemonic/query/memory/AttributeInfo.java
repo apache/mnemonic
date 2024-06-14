730c217755fa82110d17898f882b71182bcfa1fb
Katarina <katarinaking361@gmail.com>
2024-06-14 00:43:12 +0000

    MNEMONIC-831: Enhanced Code Documentation

--- mnemonic-common/src/main/java/org/apache/mnemonic/query/memory/AttributeInfo.java
+++ mnemonic-common/src/main/java/org/apache/mnemonic/query/memory/AttributeInfo.java
@@ -18,44 +18,92 @@ package org.apache.mnemonic.query.memory;
 
 import org.apache.mnemonic.DurableType;
 
+/**
+ * This class represents information about an attribute, including its name,
+ * type, sort order, and entity field ID.
+ */
 public class AttributeInfo {
 
+  // The name of the attribute
   private String name;
 
+  // The type of the attribute, represented by a DurableType
   private DurableType type;
 
+  // The sort order of the attribute
   private SortOrder sortOrder;
 
+  // The ID of the entity field associated with this attribute
   private long entityFieldId;
 
+  /**
+   * Gets the name of the attribute.
+   * 
+   * @return the name of the attribute
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Sets the name of the attribute.
+   * 
+   * @param name the new name of the attribute
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Gets the sort order of the attribute.
+   * 
+   * @return the sort order of the attribute
+   */
   public SortOrder getSortOrder() {
     return sortOrder;
   }
 
+  /**
+   * Sets the sort order of the attribute.
+   * 
+   * @param sortOrder the new sort order of the attribute
+   */
   public void setSortOrder(SortOrder sortOrder) {
     this.sortOrder = sortOrder;
   }
 
+  /**
+   * Gets the type of the attribute.
+   * 
+   * @return the type of the attribute
+   */
   public DurableType getType() {
     return type;
   }
 
+  /**
+   * Sets the type of the attribute.
+   * 
+   * @param type the new type of the attribute
+   */
   public void setType(DurableType type) {
     this.type = type;
   }
 
+  /**
+   * Gets the entity field ID associated with the attribute.
+   * 
+   * @return the entity field ID
+   */
   public long getEntityFieldId() {
     return entityFieldId;
   }
 
+  /**
+   * Sets the entity field ID associated with the attribute.
+   * 
+   * @param entityFieldId the new entity field ID
+   */
   public void setEntityFieldId(long entityFieldId) {
     this.entityFieldId = entityFieldId;
   }
