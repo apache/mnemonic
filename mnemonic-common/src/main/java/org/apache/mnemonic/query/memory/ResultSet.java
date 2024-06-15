c3e4fe5dc95189c7cd8344ff5e9332a74c6558bb
Katarina <katarinaking361@gmail.com>
2024-06-15 19:04:21 +0000

    MNEMONIC-833: Added Code Comments

--- mnemonic-common/src/main/java/org/apache/mnemonic/query/memory/ResultSet.java
+++ mnemonic-common/src/main/java/org/apache/mnemonic/query/memory/ResultSet.java
@@ -20,24 +20,48 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.util.Iterator;
 
+/**
+ * ResultSet class representing the results of a query.
+ * Implements Iterable<Long> for iterating over the results and Closeable for resource management.
+ */
 public class ResultSet implements Iterable<Long>, Closeable {
 
+  // The Queryable instance used to execute the query
   private Queryable m_queryable;
 
+  // The query ID associated with this ResultSet
   private long m_qid;
 
+  /**
+   * Constructor to initialize the ResultSet with a Queryable instance and a query ID.
+   *
+   * @param queryable the Queryable instance used to execute the query
+   * @param qid the query ID associated with this ResultSet
+   */
   public ResultSet(Queryable queryable, long qid) {
     m_queryable = queryable;
     m_qid = qid;
   }
 
+  /**
+   * Returns an iterator over the results.
+   * Currently returns null, should be implemented to return actual results.
+   *
+   * @return an Iterator<Long> over the results
+   */
   @Override
   public Iterator<Long> iterator() {
-    return null;
+    return null; // Placeholder, should return an actual iterator
   }
 
+  /**
+   * Closes the ResultSet and releases any resources associated with it.
+   * Currently does nothing, should be implemented to handle resource cleanup.
+   *
+   * @throws IOException if an I/O error occurs
+   */
   @Override
   public void close() throws IOException {
-
+    // Placeholder, should implement resource cleanup
   }
 }
