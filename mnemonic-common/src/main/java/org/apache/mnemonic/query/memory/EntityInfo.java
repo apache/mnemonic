95e20e405442b678f49e48af00970e8a55b66f6e
Katarina <katarinaking361@gmail.com>
2024-06-15 01:20:52 +0000

    MNEMONIC-832: EntityInfo Class Comments

--- mnemonic-common/src/main/java/org/apache/mnemonic/query/memory/EntityInfo.java
+++ mnemonic-common/src/main/java/org/apache/mnemonic/query/memory/EntityInfo.java
@@ -16,34 +16,71 @@
  */
 package org.apache.mnemonic.query.memory;
 
+/**
+ * The EntityInfo class represents metadata about an entity in the Mnemonic framework.
+ * It holds information about the entity's class name, entity name, and its attributes.
+ */
 public class EntityInfo {
 
+  // The fully qualified class name of the entity.
   private String className;
 
+  // The name of the entity.
   private String entityName;
 
+  // An array of AttributeInfo objects, representing the attributes of the entity.
   private AttributeInfo[] attributeInfo;
 
+  /**
+   * Gets the class name of the entity.
+   *
+   * @return the class name as a String.
+   */
   public String getClassName() {
     return className;
   }
 
+  /**
+   * Sets the class name of the entity.
+   *
+   * @param className the class name to set.
+   */
   public void setClassName(String className) {
     this.className = className;
   }
 
+  /**
+   * Gets the name of the entity.
+   *
+   * @return the entity name as a String.
+   */
   public String getEntityName() {
     return entityName;
   }
 
+  /**
+   * Sets the name of the entity.
+   *
+   * @param entityName the entity name to set.
+   */
   public void setEntityName(String entityName) {
     this.entityName = entityName;
   }
 
+  /**
+   * Gets the array of AttributeInfo objects associated with the entity.
+   *
+   * @return an array of AttributeInfo objects.
+   */
   public AttributeInfo[] getAttributeInfo() {
     return attributeInfo;
   }
 
+  /**
+   * Sets the array of AttributeInfo objects for the entity.
+   *
+   * @param attributeInfo an array of AttributeInfo objects to set.
+   */
   public void setAttributeInfo(AttributeInfo[] attributeInfo) {
     this.attributeInfo = attributeInfo;
   }
